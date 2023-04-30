3effeb8eb02d8181ce0798b28171c640916ef853
Alan Diaz Lamberti <alan.diazlamberti@gmail.com>
2023-04-30 13:51:55 -0300

    prueba

---
+++ M1-U4/prueba/prueba.js
@@ -0,0 +1,84 @@
+// --------------------------------------------------------------------
+// PRUEBA DE CONSOLA Y DOCUMENTO
+// console.log("Hola")
+// console.log(2+2)
+// console.log("2 + 2 son", 2+2, ",4 y 2 son", 4+2)
+// document.write("Hola")
+
+// --------------------------------------------------------------------
+// PRUEBA DE VARIABLES
+
+// const nombre = "Alan"
+// var edad = 24
+
+// console.log(nombre, edad);
+// document.write(nombre + " " + edad);
+
+// -------------------------------------------------------------------
+// PRUEBA DE FUNCION PROMPT
+
+// const nombre = prompt("Ingrese su nombre", "")
+// console.log("Su nombre es", nombre)
+// document.write("Su nombre es" + " " + nombre)
+
+// -------------------------------------------------------------------
+// PRUEBA DE ARRAYS
+
+// let dias_de_la_semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+// let dia_seleccionado = dias_de_la_semana[2]
+
+// console.log(dia_seleccionado)
+// document.write(dia_seleccionado)
+
+// -------------------------------------------------------------------
+// PRUEBA DE ESTRUCTURAS DE CONTROL
+
+// let edad = 18
+// if(edad >= 18){
+//     console.log("Soy mayor de edad")
+//     document.write("Soy mayor de edad")
+// }
+
+// let edad = 18
+// if(edad >= 18){
+//     console.log("Soy mayor de edad")
+//     document.write("Soy mayor de edad")
+// }
+// else{
+//     console.log("No soy mayor de edad")
+//     document.write("No soy mayor de edad")
+// }
+
+// let nota = 10;
+
+// switch (nota) {
+//     case 1: nota = "Reprobado"; break;
+//     case 2:
+//     case 3: nota = "Malo"; break;
+//     case 4:
+//     case 5: nota = "Regular"; break;
+//     case 6:
+//     case 7: nota = "Bien"; break;
+//     case 8:
+//     case 9: nota = "Muy bien"; break;
+//     case 10: nota = "Excelente"; break;
+//     default: nota = "Ingrese un numero entero del 1 al 10"; break;
+// }
+
+// console.log(nota)
+
+// -------------------------------------------------------------------
+// PRUEBA DE BUCLES
+
+// const mensaje = "Ayuda, estoy dentro de un bucle"
+
+// for(let i=0; i<3 ; i++){
+//     alert(mensaje)
+// }
+
+// let x = 1;
+
+// while(x <= 100){
+//     document.write(x + "<br>");
+//     x++;
+// }
