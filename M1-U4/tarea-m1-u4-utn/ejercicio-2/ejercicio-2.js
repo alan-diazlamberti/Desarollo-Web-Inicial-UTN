e218f9194c1d235d7e3942e1d75c301d52dd0390
Alan Diaz Lamberti <alan.diazlamberti@gmail.com>
2023-05-01 22:40:44 -0300

    finalizado ejercicio 2

--- M1-U4/tarea-m1-u4-utn/ejercicio-2/ejercicio-2.js
+++ M1-U4/tarea-m1-u4-utn/ejercicio-2/ejercicio-2.js
@@ -1,3 +1,18 @@
 // Ejercicio 2
 
 // Haciendo uso de los bucles, recorrer un array de números y determinar cual es el mayor
+
+let numeros = [3, 6, 9, 12, 15, 12, 9, 6, 3]
+
+let numeroMayor = numeros[0]
+
+for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] > numeroMayor){
+        numeroMayor = numeros[i]
+    }
+}
+
+console.log("Recorremos el array de números:", numeros)
+console.log("El número mayor es:", numeroMayor)
+document.write("Recorremos el array de números:" + " " + numeros + "<br>")
+document.write("El número mayor es:" + " " + numeroMayor)
